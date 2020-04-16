9e636277d7bad7b68242147bbd61960cac85efe0
Rony <ronymawad@gmail.com>
2020-04-16 15:59:14 -0400

    make it better

--- api/routes/users.js
+++ api/routes/users.js
@@ -23,6 +23,24 @@ User.find({$or : [{role : "S"}, {role : "P"}]})
 });
 });
 
+
+router.get('/sup',(req,res,next)=>{
+    User.find({$or : [{role : "P"}, {role : "A"}]})
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        if(doc){
+        res.status(200).json(doc);
+        } else{
+            res.status(404).json({message : 'No such USER for this ROLE'})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error : err})
+    });
+    });
+
 router.post('/signup', (req,res,next)=>{
     User.find({email : req.body.email})
     .exec()
